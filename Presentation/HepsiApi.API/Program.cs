7036f761cb4bdadbf809a4b0586ef083ad24df6c
kubraacelik <kkbra.celik92@gmail.com>
2024-09-28 12:59:05 +0300

    added AddPersistence

--- Presentation/HepsiApi.API/Program.cs
+++ Presentation/HepsiApi.API/Program.cs
@@ -1,16 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using HepsiApi.Persistence;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// uygulama farklý ortamlar için özelleþtirilmiþ ayarlarla çalýþtýrýlabilir.
 var env = builder.Environment;
-
 builder.Configuration
     .SetBasePath(env.ContentRootPath)
     .AddJsonFile("appsettings.json", optional: false)
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+// uygulamanýn hizmetlerine veritabaný baðlamýný (DbContext) ekler.
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
