5742b83c3db91d18bd479a0de66651def3c87cc6
kubraacelik <kkbra.celik92@gmail.com>
2024-10-04 14:10:11 +0300

    added product information to be returned as a result of the query

---
+++ Core/HepsiApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace HepsiApi.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryResponse
+    {
+        // her bir ürün için hangi bilgilerin kullanıcıya döndürüleceğini belirtir.
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discound { get; set; }
+    }
+}
