fef222af6af8bc65771fc779e6e993248e2dcbc3
kubraacelik <kkbra.celik92@gmail.com>
2024-10-04 14:09:21 +0300

    query operation for listing products has been added

---
+++ Core/HepsiApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace HepsiApi.Application.Features.Products.Queries.GetAllProducts
+{
+    // CQRS deseninde query işlemi için kullanılan bir Request (istek) sınıfıdır.
+    // Bu sınıf, ürünlerin listelenmesi amacıyla yapılan sorgu talebini temsil eder.
+    public class GetAllProductsQueryRequest : IRequest<IList<GetAllProductsQueryResponse>>
+    {
+    }
+}
