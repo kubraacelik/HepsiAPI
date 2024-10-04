bbb75ae0f09d0203ab79c0a24dd5c5f5cd87b1c4
kubraacelik <kkbra.celik92@gmail.com>
2024-10-04 14:14:57 +0300

    Added components to manage query and command operations with MediatR

---
+++ Core/HepsiApi.Application/Registration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application
+{
+    // Dependency Injection ile gerekli servisleri kaydetmek için bir extension method tanımlıyor.
+    public static class Registration
+    {
+        public static void AddApplication(this IServiceCollection services) 
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assembly));
+        }
+    }
+}
