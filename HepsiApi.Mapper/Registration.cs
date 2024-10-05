d246a23493f5a0b665dd7fbcd0463eab16ba2928
kubraacelik <kkbra.celik92@gmail.com>
2024-10-05 16:55:28 +0300

    added automapper with dependeny injection

---
+++ HepsiApi.Mapper/Registration.cs
@@ -0,0 +1,14 @@
+﻿using HepsiApi.Application.Interfaces.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HepsiApi.Mapper
+{
+    public static class Registration
+    {
+        //  Dependency Injection yapısını kullanarak ICustomMapper arayüzünü ve CustomMapper sınıfını servislere ekler
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+                services.AddSingleton<ICustomMapper, AutoMapper.CustomMapper>();
+        }
+    }
+}
