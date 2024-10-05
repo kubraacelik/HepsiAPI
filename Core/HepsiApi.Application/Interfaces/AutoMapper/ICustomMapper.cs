68c33566619bfb192b61e9f9170893f94dba32f9
kubraacelik <kkbra.celik92@gmail.com>
2024-10-05 16:53:08 +0300

    added AutoMapper

---
+++ Core/HepsiApi.Application/Interfaces/AutoMapper/ICustomMapper.cs
@@ -0,0 +1,15 @@
+﻿namespace HepsiApi.Application.Interfaces.AutoMapper
+{
+    // nesneler arasında dönüşüm (mapping) işlemleri yapmayı hedefliyor. 
+    public interface ICustomMapper
+    {
+        // TSource türündeki bir nesneyi alıp TDestination türüne dönüştürür
+        TDestination Map<TDestination, TSource>(TSource source, string? ignore = null);
+        // TSource türünde bir listeyi TDestination türündeki bir listeye dönüştürür
+        IList<TDestination> Map<TDestination, TSource>(IList<TSource> source, string? ignore = null);
+        // object türündeki kaynağı TDestination türüne dönüştürür
+        TDestination Map<TDestination>(object source, string? ignore = null);
+        // object türündeki bir listeyi TDestination türündeki bir listeye dönüştürür
+        IList<TDestination> Map<TDestination>(IList<object> source, string? ignore = null);
+    }
+}
